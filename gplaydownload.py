38a04bebe0d1fe2345c9b17e8e3e8a3975a689bb
mikefinkelnyt <michael.finkel@nytimes.com>
2013-09-13 14:21:50 -0400

    Almost done

--- gplaydownload.py
+++ gplaydownload.py
@@ -3,7 +3,6 @@ import getopt
 import os
 import os.path
 
-
 from gmusicapi import Musicmanager
 
 def usage():
@@ -26,32 +25,39 @@ def parse_args():
         download_dir += '/'
     return download_dir
 
-download_dir = parse_args()
+def get_songs():
+    print "Checking for updates"
+    songs = mm.get_all_songs()
+    for song in songs:
+        path = download_dir + song['artist'][0] + '/' + song['artist'] + '/' + song['album']
+        path = path.lower().replace(' ', '_')
+        track_number = str(song['track_number'])
+        if len(track_number) < 2:
+            track_number = '0' + track_number
+        filename = track_number + ' ' + song['title'] + '.mp3'
+        filename = filename.lower().replace(' ', '_')
+        full_path = path + '/' + filename
+        if not os.path.isfile(full_path): 
+            if not os.path.isdir(path):
+                print "Creating directory " + path
+                os.makedirs(path)
+            print "Downloading " + full_path
+            suggested_filename, audio = mm.download_song(song['id'])
+            with open(full_path, 'wb') as f:
+                f.write(audio)
 
+def run():
+    global download_dir, mm
+    download_dir = parse_args()
+    print "Creating music manager"
+    mm = Musicmanager()
+    print "Attempting login"
+    if not mm.login():
+        print "OAuth required:"
+        mm.perform_oauth()
 
-print "Creating music manager"
-mm = Musicmanager()
-print "Attempting login"
-if not mm.login():
-    print "OAuth required:"
-    mm.perform_oauth()
-songs = mm.get_all_songs()
-for song in songs:
-    path = download_dir + song['artist'][0] + '/' + song['artist'] + '/' + song['album']
-    path = path.lower().replace(' ', '_')
-    track_number = str(song['track_number'])
-    if len(track_number) < 2:
-        track_number = '0' + track_number
-    filename = track_number + ' ' + song['title'] + '.mp3'
-    filename = filename.lower().replace(' ', '_')
-    full_path = path + '/' + filename
-    if not os.path.isfile(full_path): 
-        if not os.path.isdir(path):
-            print "Creating directory " + path
-            os.makedirs(path)
-        print "Downloading " + full_path
-        suggested_filename, audio = mm.download_song(song['id'])
-        with open(full_path, 'wb') as f:
-            f.write(audio)
+    get_songs()
 
+if __name__ == "__main__": 
+    run()
 
